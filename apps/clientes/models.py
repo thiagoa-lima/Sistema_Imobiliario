c2299a1fe7dd8c1e5ca33c990fe7b9b2da991306
thiagoandradelima <thiagog_12@hotmail.com>
2023-01-24 17:03:43 -0300

    Inclusão de novos campos no cadastro de clientes

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 class Clientes(models.Model):
     sexo_choices = (('M', 'Masculino'), ('F', 'Feminino'), ('Indef', 'Não quero informar'))
-
+    qualificacao_choices = (('L', 'Locatário'), ('P', 'Proprietário'), ('F', 'Fiador'))
+    tipo_cliente_choices = ((1, 'Pessoa Física'), (2, 'Pessoa Jurídica'))
+    
+    tipo_cliente = models.CharField('Tipo Cliente', max_length=20, choices=tipo_cliente_choices, default=1)
+    qualificacao = models.CharField('Qualificação', max_length=15, choices=qualificacao_choices, default="L")
     nome = models.CharField('Nome', max_length=120)
     sexo = models.CharField('Sexo', max_length=10, choices=sexo_choices, default='Indef')
     email = models.EmailField('E-mail', max_length=50)
