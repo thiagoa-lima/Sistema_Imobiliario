b2e311914e4a68c7e18acd6b480c39a8f7483397
thiagoandradelima <thiagog_12@hotmail.com>
2023-02-03 18:08:04 -0300

    inlcusao da pasta static

--- apps/cadastros/views.py
+++ apps/cadastros/views.py
@@ -4,6 +4,9 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+# class IndexView(TemplateView):
+#     template_name = 'index.html'
+
 
 # from django.shortcuts import render
 
