bb6f3ca5a7fce2aeb8b5a472a55cd979c295f3d9
thiagoandradelima <thiagog_12@hotmail.com>
2023-02-04 20:51:44 -0300

    criação de um template base

--- apps/cadastros/urls.py
+++ apps/cadastros/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, SobreView, BootView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('sobre/', SobreView.as_view(), name='sobre'),
+    path('boot/', BootView.as_view(), name='boot'),
 ]
--- apps/cadastros/views.py
+++ apps/cadastros/views.py
@@ -4,6 +4,13 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+class SobreView(TemplateView):
+    template_name = 'sobre.html'
+
+class BootView(TemplateView):
+    template_name = 'boot.html'
+
+
 # class IndexView(TemplateView):
 #     template_name = 'index.html'
 
