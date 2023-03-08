977e80b2f5cc7018e6c08ca525647efac3a016d8
thiagoandradelima <thiagog_12@hotmail.com>
2023-03-08 14:34:25 -0300

    Detalhes login na base.

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
-    # path('login/', auth_views.LoginView.as_view(template_name = 'usuarios/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
    
     path('usuarios/', Usuarios_List.as_view(), name='usuarios-listar'),
