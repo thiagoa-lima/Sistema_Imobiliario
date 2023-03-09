5df035a5a6332a7a02d8f485677a4a8f25960869
thiagoandradelima <thiagog_12@hotmail.com>
2023-03-09 16:43:15 -0300

    Atualização da base.html

--- apps/administrativo/models.py
+++ apps/administrativo/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class Painel_administrativo(models.Model):
     
     # Dados Cadastrais
-    cpf_cnpj = models.CharField("CPF/CNPJ", max_length=100, null=True)
-    nome_empresa = models.CharField("Nome da empresa", max_length=100, null=True)
-    nome_fantasia = models.CharField("Nome fantasia", max_length=100, null=True)
+    cpf_cnpj = models.CharField("CPF/CNPJ", max_length=100, null=True, blank=True)
+    nome_empresa = models.CharField("Nome da empresa", max_length=100, null=True, blank=True)
+    nome_fantasia = models.CharField("Nome fantasia", max_length=100, null=True, blank=True)
 
     # Contato
     telefone = models.CharField("Telefone", max_length=20, null=True, blank=True)
@@ -24,8 +24,8 @@ class Painel_administrativo(models.Model):
     uf = models.CharField('UF', max_length=2, null=True, blank=True)
 
     # Taxas
-    multa = models.DecimalField("Taxa de multa (%)", decimal_places=2, max_digits=5, null=True)
-    juros_mensal = models.DecimalField("Taxa de juros mensal (%)", decimal_places=2, max_digits=5, null=True)
+    multa = models.DecimalField("Taxa de multa (%)", decimal_places=2, max_digits=5, null=True, blank=True)
+    juros_mensal = models.DecimalField("Taxa de juros mensal (%)", decimal_places=2, max_digits=5, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.nome_fantasia
