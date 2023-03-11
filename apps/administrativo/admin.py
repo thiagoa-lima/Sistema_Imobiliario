5521d4ed6a91b19c501f3efd250de66f09c15920
thiagoandradelima <thiagog_12@hotmail.com>
2023-03-11 15:56:23 -0300

    admin app administrativo

--- apps/administrativo/admin.py
+++ apps/administrativo/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from apps.administrativo.models import Painel_administrativo
 # Register your models here.
 
-
 @admin.register(Painel_administrativo)
 class Painel_Admin(admin.ModelAdmin):
     list_display = ('cpf_cnpj', 'nome_empresa', 'nome_fantasia')
